6a51008ff6cf1e7973d554fbd1a47af315548d92
dev.mhrony <mh.rony.dhanvi@gmail.com>
2023-04-27 20:15:11 +0600

    first commit

---
+++ include/config.php
@@ -0,0 +1,23 @@
+<?php
+$db_host = "localhost";
+$db_name = "portfolio";
+$db_pass = "";
+$db_user = "root";
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+?>
+
+<!--
+---------------------------------------------------------------
+
+/*
+Template       :  Code Camp BD - Meditach
+Author         :  MH RONY
+GitHub         :  https://github.com/dev-mhrony
+Facebook       :  https://facebook.com/dev.mhrony
+Website        :  https://developerrony.com
+Linkdin        :  https://www.linkedin.com/in/dev-mhrony
+*/
+---------------------------------------------------------------
+-->
